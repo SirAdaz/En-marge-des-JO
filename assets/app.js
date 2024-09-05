29b75922567202759b55c2ac7a11c207fb2690ad
Adaz SirAdaz <mrxptdrgame@gmail.com>
2024-09-05 10:34:56 +0200

    calendar non personalisable

--- assets/app.js
+++ assets/app.js
@@ -25,3 +25,11 @@ window.onload = function () {
         });
     }
 };
+
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth'
+    });
+    calendar.render();
+  });
